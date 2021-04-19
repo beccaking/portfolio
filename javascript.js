f6a188f2bf135ee57303eb5b17b7a8b074070394
beccaking <rebecca.lynne.king@gmail.com>
2021-04-18 21:08:00 -0400

    Created site header and carousel

---
+++ javascript.js
@@ -0,0 +1,45 @@
+if(window.innerWidth >750){
+  $('#logo').append($('ul'))
+}
+
+$(window).resize(function(){
+  if(window.innerWidth > 750){
+    $('#logo').append($('ul'))
+  } else {
+    $('main').prepend($('ul'))
+  }
+})
+
+let i = 0
+let cards = document.querySelectorAll('.carousel div')
+let dots = document.querySelectorAll('.carousel-dots span')
+
+function rotate () {
+
+  if(cards[cards.length - 1].classList.contains('active')){
+    cards[cards.length - 1].classList.remove('active')
+    dots[dots.length - 1].classList.remove('active')
+    i = 0
+  } else {
+    cards[i].classList.remove('active')
+    dots[i].classList.remove('active')
+    i++
+  }
+  dots[i].classList.add('active')
+  cards[i].classList.add('active')
+}
+
+let cardInterval = setInterval(rotate, 5000);
+
+// Set the interval to stop when you click an off-screen button
+// Allow user to select one of the cards by clicking the dot
+// for(j=0;j<cards.length;j++){
+//
+//   dots[j].addEventListener('click', function(){
+//
+//     $('.carousel div').removeClass('active')
+//     $('.carousel-dots span').removeClass('active')
+//
+//     $(this).addClass('active')
+//   })
+// }
