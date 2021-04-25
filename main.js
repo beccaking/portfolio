c3ae2d59fd3c025aedad2b3ce746f6cab1c5202d
beccaking <rebecca.lynne.king@gmail.com>
2021-04-25 17:12:16 -0400

    Homepage updates

--- main.js
+++ main.js
@@ -9,37 +9,6 @@ $(window).resize(function(){
     $('#logo').append($('.links'))
   } else {
     $('main').prepend($('ul'))
-    $('footer').append($('.links'))
+    $('main').append($('.links'))
   }
-})
-
-let i = 0
-let cards = document.querySelectorAll('.carousel div')
-let dots = document.querySelectorAll('.carousel-dots span')
-
-function rotate () {
-
-  if(cards[cards.length - 1].classList.contains('active')){
-    cards[cards.length - 1].classList.remove('active')
-    dots[dots.length - 1].classList.remove('active')
-    i = 0
-  } else {
-    cards[i].classList.remove('active')
-    dots[i].classList.remove('active')
-    i++
-  }
-  dots[i].classList.add('active')
-  cards[i].classList.add('active')
-}
-
-let cardInterval = setInterval(rotate, 5000);
-
-// Set the interval to stop when you click an off-screen button
-
-$('.off-screen').on('click', function(){
-  clearInterval(cardInterval)
-})
-
-$('.carousel').on('drag', function(){
-  $('.carousel div').show();
 })
