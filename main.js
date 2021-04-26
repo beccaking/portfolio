eb60e46b59515bb3f96e6520050c303c558c093e
beccaking <rebecca.lynne.king@gmail.com>
2021-04-25 20:34:56 -0400

    Portfolio Site Version 1 Complete

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
 if($(window).width()> 750){
   $('#logo').append($('ul'))
-  $('#logo').append($('.links'))
 }
 
 $(window).resize(function(){
   if($(window).width() > 750){
     $('#logo').append($('ul'))
-    $('#logo').append($('.links'))
   } else {
     $('main').prepend($('ul'))
-    $('main').append($('.links'))
+  }
+})
+
+$('h4').on('click keypress', function(e){
+  if(e.keyCode = 13 || $(this).data('clicked', true)){
+    $('h4').removeClass('active')
+    $(this).addClass('active')
+    $('.project').removeClass('active')
+
+    if($(this).is('#amerimark-heading')){
+      $('#amerimark').addClass('active')
+    }
+    if($(this).is('#ncm-heading')){
+      $('#ncm').addClass('active')
+    }
+    if($(this).is('#ga-heading')){
+      $('#ga').addClass('active')
+    }
   }
 })
